471a1220fa5ad2648d81a65c7085127a0d1c9873
AdityaGaneshRaskar <131153151+AdityaGaneshRaskar@users.noreply.github.com>
2023-04-18 15:55:33 +0530

    Create largestof3numbers.py

---
+++ largestof3numbers.py
@@ -0,0 +1,11 @@
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=int(input("enter the number:"))
+if a>b:
+    print(a," is the largest number")
+elif b>c:
+    print(b," is the largest number")
+elif a>c:
+    print(c," is the largest number")
+else:
+    print("none")
